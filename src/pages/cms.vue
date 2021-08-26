9b0b8de5ae3771741030533891529f20423864b2
Rleaf <calcifer44@gmail.com>
2021-08-26 03:27:19 -0400

    UX / Architecture for blog page

--- src/pages/cms.vue
+++ src/pages/cms.vue
@@ -1,15 +1,27 @@
 <template>
-  <div id="landing">
-     <NavBar />
-     <div id="textContainerHeader">
-     </div>
-     <div id="curriculumBody">
-      <li v-for="restaurant in restaurants" :key="restaurant.id">
+   <div id="landing">  
+      <NavBar />
+      <div id="textContainerHeader">
+      </div>
+      <div id="curriculumBody">
+         <div id="curriculumHeader">
+            <i style="font-size: 18px;">---</i>
+         </div>
+         <p>A place to record my thoughts.</p>
+         <div id="curriculumBody">
+            <li v-for='blog in blogs' :key="blog.id">
+               <a href="http://www.google.com">{{ blog.name }}</a>
+            </li>
+            <!-- <li v-for='blog in blogs' :key="blog.id">
+               <a href="http://www.google.com">{{ blog.name }}</a>
+            </li> -->
+         </div>
+      <!-- <li v-for="restaurant in restaurants" :key="restaurant.id">
         {{ restaurant.name }}
         {{ restaurant.description }}
         <br>
         
-      </li>
+      </li> -->
       
 
      </div>
@@ -27,14 +39,14 @@ export default {
    },
    data () {
       return {
-         restaurants: [],
+         blogs: [],
          error: null
       }
    },
    async mounted () {
       try {
-         const response = await axios.get('http://localhost:1337/restaurants')
-         this.restaurants = response.data
+         const response = await axios.get('http://localhost:1337/blogs')
+         this.blogs = response.data
          console.log(response);
       } catch (error) {
          this.error = error;
@@ -43,6 +55,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+#curriculumBody > a, li {
+   padding: 10px 0 10px 0;
+   list-style-type: none;
+   text-decoration: underline;
+}
 
 </style>
