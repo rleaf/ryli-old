aa400ca2ad743f724a9bef6b0fd5adabce221e50
Rleaf <calcifer44@gmail.com>
2021-12-27 18:58:40 -0500

    ty skiena

--- src/components/blog/bigo.vue
+++ src/components/blog/bigo.vue
@@ -13,7 +13,7 @@
                </div>
             </div>
             <p>
-               A main benefit of <a href="https://en.wikipedia.org/wiki/Big_O_notation">"Big Oh"</a> notation is that it provides a way to analyze an algorithm's runtime as the input for that algorithm increases.
+               <a href="https://en.wikipedia.org/wiki/Big_O_notation">"Big Oh"</a> notation provides a way to analyze an algorithm's runtime as the input for that algorithm increases.
                I refer to this relationship, an algorithm's runtime and the input size of said algorithm, when I talk about the <i>efficiency</i> of the algorithm. Fueled on pure intuition, it is natural to presume an algorithm
                to take increasingly longer to complete as the input gets progressively larger.
                <br>
@@ -26,23 +26,61 @@
                Immediately resiging because of how long it would take to locate Waldo, the only feeling you are left with is how perturbed you are knowing your coworker owns a 22,000+ page <i>Where's Waldo</i> children's book.
                <br>
                <br>
-               As humans (the algorithm), it is easy to scan a page (the input) and find resolution in how quickly it will take you to complete a desired task. Notice though, how impractical it becomes for us as the page count
-               gets larger. At some arbitrary page count, most people would not bother doing the task. This is exactly the discussion of "Big Oh" and <a href="https://en.wikipedia.org/wiki/Time_complexity" target="_blank">time complexity</a>.
-               Once again, these concepts provide a means to understand the asymptotic growth - the behavior of a function as the input gets larger.
-               Having the ability to quantify an algorithm's efficiency provides extended benefits. Not only can we learn more about that algorithm itself, but we can compare two algorithms to each other to see which one is more
-               efficient.
+               As humans (the algorithm), it is easy to scan a page (the input) and find solace in how quickly it will take you to complete a desired task. Notice though, how impractical it becomes for us as the page count
+               gets larger. At some arbitrary page count, most people would not bother doing the task because it gets so ridiculous. This is exactly the discussion of "Big Oh" and <a href="https://en.wikipedia.org/wiki/Time_complexity" target="_blank">time complexity</a>.
+               Once again, these concepts provide a means to understand the asymptotic growth of functions/algorithms - the behavior of a function/algorithm as the input gets larger. Similar to the derivative, when we discuss "Big Oh", we are interested in the <i>rate of change</i>.
+               I say this not to dismiss questions that ask specifically about a function's value at any point in it's domain, just to provide pedagogical "direction" for understanding "Big Oh". Asking the efficiency of an algorithm when fed an arbitrarily sized input is also valuable.
+               For example if there are two competing algorithms with similar efficiency, a valuable question could be "at what point does algorithm <i>x</i> become more efficient than algorithm <i>y</i>?"
             </p>
             <div id="blogSubHeader">
                The Definitions
             </div>
+            <br>
+            <br>
+            <img id="img1000" style="box-shadow: none;" src="../../assets/blog/bigo3.png" alt="">
             <p>
-               <vue-mathjax :formula='bigO'></vue-mathjax> means <vue-mathjax :formula='`$c \\cdot g(n)$`'></vue-mathjax> is an <i>upper bound</i> on <vue-mathjax :formula='`$f(n)$`'></vue-mathjax>. Thus there exists
-               some constant <i>c</i> such that <vue-mathjax :formula='`$f(n)$`'></vue-mathjax> is always <vue-mathjax :formula='`$\\leq c \\cdot g(n)$`'></vue-mathjax>, for large enough <i>n</i>.
+               (a) <vue-mathjax :formula='bigO'></vue-mathjax> means <vue-mathjax :formula='`$c \\cdot g(n)$`'></vue-mathjax> is an <i>upper bound</i> on <vue-mathjax :formula='`$f(n)$`'></vue-mathjax>. Thus there exists
+               some constant <i>c</i> such that <vue-mathjax :formula='`$f(n)$`'></vue-mathjax> is always <vue-mathjax :formula='`$\\leq c \\cdot g(n)$`'></vue-mathjax>, for large enough <vue-mathjax :formula='`$n\\geq n_0$`'></vue-mathjax>.
                <!-- We say a function <vue-mathjax :formula='`$f(n)$`'></vue-mathjax> belongs to "Big Oh" of the function g when <vue-mathjax :formula='bigO'></vue-mathjax> -->
                <br>
                <br>
-               <vue-mathjax :formula='bigTheta'></vue-mathjax> means <vue-mathjax :formula='`$c \\cdot g(n)$`'></vue-mathjax> is an <i>upper bound</i> on <vue-mathjax :formula='`$f(n)$`'></vue-mathjax>. Thus there exists
-               some constant <i>c</i> such that <vue-mathjax :formula='`$f(n)$`'></vue-mathjax> is always <vue-mathjax :formula='`$\\leq c \\cdot g(n)$`'></vue-mathjax>, for large enough <i>n</i>.
+               (b) <vue-mathjax :formula='bigOmega'></vue-mathjax> means <vue-mathjax :formula='`$c \\cdot g(n)$`'></vue-mathjax> is a <i>lower bound</i> on <vue-mathjax :formula='`$f(n)$`'></vue-mathjax>. Thus there exists
+               some constant <i>c</i> such that <vue-mathjax :formula='`$f(n)$`'></vue-mathjax> is always <vue-mathjax :formula='`$\\geq c \\cdot g(n)$`'></vue-mathjax>, for large enough <vue-mathjax :formula='`$n\\geq n_0$`'></vue-mathjax>.
+               <br>
+               <br>
+               (c) <vue-mathjax :formula='bigTheta'></vue-mathjax> means <vue-mathjax :formula='`$c_1 \\cdot g(n)$`'></vue-mathjax> is an <i>upper bound</i> on <vue-mathjax :formula='`$f(n)$`'></vue-mathjax> and
+               <vue-mathjax :formula='`$c_2 \\cdot g(n)$`'></vue-mathjax> is a <i>lower bound</i> on <vue-mathjax :formula='`$f(n)$`'></vue-mathjax>. Thus there exists constants <vue-mathjax :formula='`$c_1$`'></vue-mathjax>
+               and <vue-mathjax :formula='`$c_2$`'></vue-mathjax> such that <vue-mathjax :formula='`$c_2 \\cdot g(n) \\leq f(n) \\leq c_1\\cdot g(n) $`'></vue-mathjax> for large enough <vue-mathjax :formula='`$n\\geq n_0$`'></vue-mathjax>.
+               <br>
+               <br>
+               The picture and definitions are from <a href="https://www.algorist.com/" target="_blank">The Algorithm Design Manuel, by Steven Skiena</a> (p 35-36 of 2nd edition). Before explaining how to use these functions, a couple
+               of things to note: 1) Some people consider <vue-mathjax :formula='bigO'></vue-mathjax> to be an <a href="https://en.wikipedia.org/wiki/Big_O_notation#Equals_sign" target="_blank">abuse of notation</a> because we mean to say
+               "<i>f(n)</i> is a member of set <i>O(g(n))</i>" which usually is represented in set notation as <vue-mathjax :formula='`$f(x) \\in O(g(n))$`'></vue-mathjax>. 2) It doesn't seem to be stated definitively
+               in Skiena's book (or maybe I missed it), but it should seem apparent that <vue-mathjax :formula="`$c > 0$`"></vue-mathjax> otherwise we're subject to some funk. This is stated in
+               <a href="https://mitpress.mit.edu/books/introduction-algorithms-third-edition" target="_blank">Introduction to Algorithms, 3rd Ed</a> on p. 50.
+            </p>
+            <div id="blogSubHeader">
+               The Implementation
+            </div>
+            <p>
+               The applications of these functions err on the side of theory. The RAM, <i>Random Access Machine</i>, is a very simplified hypothetical computer with which brings considerations that allow
+               us to represent algorithms functionally and use "Big Oh" notation. Quoting Skiena (p. 31):
+               <br>
+               <br>
+               <i>
+                  &#8226; Each simple operation (+, *, -, =, if, call) takes exactly one time step
+                  <br>
+                  &#8226; Loops and subroutines are not considered simple operations. Instead, they
+                  are the composition of many single-step operations. It makes no sense for
+                  sort to be a single-step operation, since sorting 1,000,000 items will certainly
+                  take much longer than sorting 10 items. The time it takes to run through a
+                  loop or execute a subprogram depends upon the number of loop iterations or
+                  the specific nature of the subprogram.
+                  <br>
+                  &#8226; Each memory access takes exactly one time step. Further, we have as much
+                  memory as we need. The RAM model takes no notice of whether an item is
+                  in cache or on the disk.
+               </i> 
             </p>
          </div>
          <toTop />
@@ -72,8 +110,8 @@ export default {
          error: null,
          formula: '$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}$$',
          bigO: '$f(n) = O(g(n))$',
-         bigTheta: '$f(n) = O(g(n))$',
-         bigOmega: '$f(n) = O(g(n))$',
+         bigOmega: '$f(n) = \\Omega(g(n))$',
+         bigTheta: '$f(n) = \\Theta(g(n))$',
       }
    },
 
