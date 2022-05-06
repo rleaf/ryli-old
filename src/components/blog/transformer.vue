586c29f10ac61e899c4a0ce43a29706829b723b0
Rleaf <calcifer44@gmail.com>
2022-05-06 17:52:21 -0400

    xformers

--- src/components/blog/transformer.vue
+++ src/components/blog/transformer.vue
@@ -10,6 +10,9 @@
                   <!-- <p style="display: flex; justify-content: center;">2 3 4 </p> -->
                   <p style="font-size: 18px; padding: 0 !important; ">5 &#8226; 4 &#8226; 2022</p>
                   <!-- <p>{{ blogs[0].name }}</p> -->
+                  <p>
+                     This blog is currently being made, but feel free to follow along :)
+                  </p>
                </div>
             </div>
             <div class="blogtoc">
@@ -21,7 +24,7 @@
                      <li><a href="#xformer_embed">Embedding</a></li>
                      <li><a href="#xformer_posenc">Positional Encoding</a></li>
                   </ul>
-                  <li><a href="#xformer_dotprod">Scaled Dot Product</a></li>
+                  <li><a href="#xformer_dotprod">Scaled Dot Product Attention</a></li>
                   <li><a href="#xformer_attention">Attention</a></li>
                   <ul>
                      <li><a href="#xformer_selfattn">Self Attention</a></li>
@@ -43,28 +46,30 @@
             </div>
             <p>
                I'm going to explain, hopefully thoroughly enough, the mechanisms present in <a href="https://arxiv.org/pdf/1706.03762.pdf" target="_blank">Transformers</a>. Usually, as evident in my prior discussions
-               about network architectures, I dedicate a portion to discussing the backwards pass; I will not be doing that here. As I write this, I would like to design this read centric around the <i>Scaled Dot Product</i>
-               and <i>Attention</i> operations.
-            </p>
+               about network architectures, I dedicate a portion to discussing the backwards pass; I will not be doing that here. I believe there will already be a lot of information just discussing the Transformer
+               architecture.
             <p>
-               It is mentioned in the opening of the <i>Attention is All You Need</i> that the transformer model was introduced to ameliorate and "push the boundaries of recurrent language
-               models and encoder-decoder architectures". While this was certainly true back when transformers were new, it is worth mentioning that the impact of transformers has broached
-               well into other domains of machine learning. Computer Vision is a solid example with <a href="https://arxiv.org/pdf/2010.11929.pdf" target="_blank">Visual Transformers</a>
-               and <a href="https://arxiv.org/pdf/2103.14030.pdf" target="_blank">Swin Transformers</a>.
+               It is mentioned in the opening of the <i>Attention is All You Need</i> that the transformer model was introduced to ameliorate and <i>"push the boundaries of recurrent language
+               models and encoder-decoder architectures"</i>. While this was certainly true back when transformers were new, it is worth mentioning that the impact of transformers has broached
+               well into other domains of machine learning. <a href="https://arxiv.org/pdf/2010.11929.pdf" target="_blank">Visual Transformers</a> and <a href="https://arxiv.org/pdf/2103.14030.pdf" target="_blank">Swin Transformers</a>
+               are good examples. Although Computer Vision already has sequential tasks such as image captioning, those linked papers use the attention mechanism to process the image data itself, which is not inherently
+               sequential.
+
                
             </p>
             <p>
-               Transformers can be structured in the following:
+               Transformers can be laid out in the following:
             </p>
             <prism-editor class="codeblock" v-model="transformerArch" :highlight="highlighter" :line-numbers="true" :readonly="true"></prism-editor>
             <p>
-               By decomposing transformers as such, it becomes easier to see the constituent parts. For example, I put a lot of importance on the <i>Scaled Dot Product</i> earlier, and for good reason too - it is
-               used heavily throughout the entire model. You can see that every attention mechanism, whether it be masked, multiheaded, or cross employs the <i>Scaled Dot Product</i>.
-               Below is the visualization of a transformer from the orginitating paper (linked above).
+               By decomposing transformers as such, it becomes easier to see the constituent parts. For example, you can see there's a lot of importance on the <i>Scaled dot product attention</i> because it is
+               used heavily throughout the entire model. Every attention mechanism, whether it be masked, multiheaded, or cross employs the <i>SDP attention</i>.
+               Below is the visualization of a transformer from the orginitating paper (linked above). It is easy to see how information flows throughout the network and specifically how each attention block
+               takes in three arguments (queries, keys, values) and the skip connections to aid gradient flow.
             </p>
             <img id="img500" src="../../assets/blog/transformer.png" alt="">
             <span style="font-size:14px; padding-top: -10px;"><i>Transformer layout from "Attention is All You Need"</i></span>
-
+            <br><br>
             <div id="xformer_prep"></div>
             <div id="blogSubHeader">
                Preperation
@@ -78,16 +83,36 @@
 
             <div id="xformer_dotprod"></div>
             <div id="blogSubHeader">
-               Scaled Dot Product
+               Scaled Dot Product Attention
             </div>
             <p>
-               The "meat and potatos" 
+               Scaled dot product attention takes takes three arguments as input: <i>queries</i>, <i>keys</i>, and <i>values</i>. These inputs are transformations
+               from the prior block's output (or original input). These transformations happen using the weights initialized in the Self Attention class which we will explore in the next in
+               the <a href="#xformer_selfattn">section</a>.
             </p>
-
+            <p>
+               Supplied with our Q, K, and V (queries/keys/values respectively), scaled dot product attention on a <u>single element from the batch</u> will look like:
+            <br>
+            <br>
+            <vue-mathjax :formula='sdp'></vue-mathjax>
+               where shapes of inputs <i>Q, K, V</i> are: 
+               <br>
+               <vue-mathjax :formula='`$Q \\in R^{k \\times e}$`'></vue-mathjax>,
+               <vue-mathjax :formula='`$K \\in R^{k \\times e}$`'></vue-mathjax>,
+               <vue-mathjax :formula='`$V \\in R^{k \\times e}$`'></vue-mathjax>
+               <br>
+               <br>
+               Fortunately in code, we can perform batch operations to calculate the attention values all at once. I encourage looking at the PyTorch SDP Attention implementation found <a href="https://github.com/pytorch/pytorch/blob/master/torch/nn/functional.py#L4765" target="_blank">here</a>.
+               I will sprinkle in the corresponding PyTorch code at every section.
+            </p>
+            <prism-editor class="codeblock" v-model="scaleddotproduct" :highlight="highlighter" :line-numbers="true" :readonly="true"></prism-editor>
             <div id="xformer_attention"></div>
             <div id="blogSubHeader">
                Attention
             </div>
+            <p>
+               break time 
+            </p>
             <div id="xformer_selfattn"></div>
             <h2>Self Attention</h2>
 
@@ -116,11 +141,6 @@
             <div id="blogSubHeader">
                Thoughts
             </div>
-            <vue-mathjax :formula='formula'></vue-mathjax>
-            <vue-mathjax :formula='jacobian'></vue-mathjax>
-            <!-- <math-jax :latex="formula" />
-            <math-jax :latex="formula" :block="true" /> -->
-            <!-- <p>{{ blogs[0].description }}</p> -->
          </div>
          <toTop />
    </div>
@@ -152,7 +172,7 @@ export default {
       // MathJax
    },
    metaInfo: {
-      title: 'Toads',
+      title: 'Transformers Deconstructed and Explained',
       meta: [
          {
             name: 'author',
@@ -160,15 +180,15 @@ export default {
          },
          { 
             name: 'description',
-            content: 'Toads'
+            content: 'I try to provide a thorough explanation on the operations and overall construction of a Transformer Neural Network.'
          },
          {
             name: 'keywords',
-            content: 'toads, are, green, sometimes'
+            content: 'transformer, neural network, machine learning, attention, self attention, scaled dot product'
          },
          {
             property: 'og:description',
-            content: 'Toads'
+            content: 'I try to provide a thorough explanation on the operations and overall construction of a Transformer Neural Network.'
          }
       ]
    },
@@ -180,8 +200,7 @@ export default {
             three: 'strawberries'
          },
          error: null,
-         formula: '$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}$$',
-         jacobian: '$$\\begin{bmatrix}a & b\\\\ c & d\\end{bmatrix}$$',
+         sdp: '$$SDP\\; Attention(Q, K, V) = Softmax_{d_k}\\biggl(\\frac{Q K^\\top}{\\sqrt{d_e}}\\biggr) V$$',
          transformerArch:
 `Transformer
 
@@ -191,7 +210,7 @@ export default {
    - Encoder Block
       - MultiHeaded Attention
          Self Attention
-            Scaled Dot Product
+            Scaled Dot Product Attn
       - Feed Forward Block
          Linear -> Act -> Linear
       - Normalization
@@ -200,14 +219,46 @@ export default {
    - Decoder Block
       - Multi Headed Attention
          Self Attention
-            Scaled Dot Product
+            Scaled Dot Product Attn
       - Masked Multi Headed Attention
          Self Attention
-            Scaled Dot Product
+            Scaled Dot Product Attn
       - Feed Forward Block
          Linear -> Act -> Linear
       - Normalization
-      - Residual Connections`
+      - Residual Connections`,
+         scaleddotproduct:
+`def scaled_dot_product_attention(q: Tensor, k: Tensor, v: Tensor, mask: Tensor = None):
+   """
+   q: shape (B, K, E) where B is batch size, K is sequence length,
+      and E is the embedding dimension.
+   k: shape (B, K, E) where B is batch size, K is sequence length,
+      and E is the embedding dimension.
+   v: shape (B, K, E) where B is batch size, K is sequence length,
+      and E is the embedding dimension.
+   mask: shape (B, K, K) where B is batch size and K is sequence length.
+
+   output: shape (B, K, E) where B is batch size, K is sequence length,
+      and E is the embedding dimension.
+   softmax_weights: (B, K, K) where B is batch size and K is sequence length.
+   """
+   B, K, E = q.shape
+
+   # Similarity matrix 
+   e = torch.bmm(query, key.transpose(1, 2))
+
+   # For Multi Headed Attention
+   if mask is Not None:
+      e = e.masked_fill_(mask.to(e.device), -1e9)
+      # e[mask] = -1e9
+
+   # Attention matrix
+   softmax_weights = (e / E**(1/2)).softmax(dim=-1)
+
+   # Second batch mat-mul of weights_softmax with values
+   y = torch.bmm(softmax_weights, v)
+   
+   return y, softmax_weights`
       }
    },
    methods: {
