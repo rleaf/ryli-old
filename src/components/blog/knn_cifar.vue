022ec1b6e19e3377ef291ac883249e16a32ebc01
Rleaf <calcifer44@gmail.com>
2022-05-12 13:54:38 -0400

    small date fix

--- src/components/blog/knn_cifar.vue
+++ src/components/blog/knn_cifar.vue
@@ -33,7 +33,7 @@
                   <li><a href="#thoughts">Thoughts</a></li>
                </ul>
             </div>
-            <i>(2/18/22) Edit: Cleaned up some wording for clarity.</i>
+            <i>(4/18/22) Edit: Cleaned up some wording for clarity.</i>
             <div id="introduction"></div>
             <div id="blogSubHeader">
                Introduction
