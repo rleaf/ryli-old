0095d9337a5dbdf1dab6e4d78a54086510f6440e
Rleaf <calcifer44@gmail.com>
2022-07-12 18:00:36 -0400

    entropyjs: typo

--- src/components/blog/assets/entropy.js
+++ src/components/blog/assets/entropy.js
@@ -6,7 +6,7 @@ export default {
    uniformBernoulli: `$$\\mathbb{H}(X) = \\Bigl(\\frac{1}{2}\\log_2{\\frac{1}{2}}+\\frac{1}{2}\\log_2{\\frac{1}{2}}\\Bigr) = 1\\,\\text{bit}$$`,
    unfairBernoulli: `$$\\mathbb{H}(X) - \\Bigl(\\frac{4}{5}\\log_2{\\frac{4}{5}}+\\frac{1}{5}\\log_2{\\frac{1}{5}}\\Bigr) = 0.72192\\dots\\text{bits}$$`,
    extremeBernoulli: `$$\\mathbb{H}(X) - \\Bigl(\\frac{19}{20}\\log_2{\\frac{19}{20}}+\\frac{1}{20}\\log_2{\\frac{1}{20}}\\Bigr) = 0.28639\\dots\\text{bits}$$`,
-   crossEntropy: `$$\\mathbb{H}(P, Q) \\triangleq - \\sum_{x=1}^{X}p(X=x)\\log_2{q(X=x)}= -\\mathbb{E}_{X\\sim\\,{p}}[\\log_2{q(X=x)}] = \\mathbb{E}_{X\\sim\\,{p}}[I_{X\\sim\\,{q}}(X = x)]$$`,
+   crossEntropy: `$$\\mathbb{H}(p, q) \\triangleq - \\sum_{x=1}^{X}p(X=x)\\log_2{q(X=x)}= -\\mathbb{E}_{X\\sim\\,{p}}[\\log_2{q(X=x)}] = \\mathbb{E}_{X\\sim\\,{p}}[I_{X\\sim\\,{q}}(X = x)]$$`,
    crossEntropyExample1: `\\begin{align}
    \\mathbb{H}(p,q) & = -\\Bigl(0.15\\log_2{0.25}+0.30\\log_2{0.10}+0.20\\log_2{0.40}+0.35\\log_2{0.25}\\Bigr) \\\\
       & = 2.26096\\dots\\text{bits}
